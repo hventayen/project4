5e2ca2dc1835e02306a7fee8edd38f7ef871e11c
hventayen <hventayen@csu.fullerton.edu>
2020-12-12 21:36:17 -0800

    finished

--- CPSC 131 Project 4/UserLogin.hpp
+++ CPSC 131 Project 4/UserLogin.hpp
@@ -8,9 +8,9 @@ using namespace std;
 
 class UserLogin {
 private:
-    
+
     unordered_map< string, string > table;
-    
+
 
 public:
     UserLogin(); // default constructor
@@ -28,7 +28,7 @@ public:
     bool validateUser(const string& userName); // look up given user
 
     bool authenticateUser(const string& userName, const string& passWord);// authenticate given user and password
-       
+
 };
 
 UserLogin::UserLogin() {
@@ -37,43 +37,83 @@ UserLogin::UserLogin() {
 // COMPLETE THE FOLLOWING FUNCTIONS
 
 void UserLogin::readIn(const string& filename) {
-    
-    // TO DO
-  
+
+  // TO DO
+  std::ifstream file;
+  std::string userName;
+  std::string passWord;
+  file.open(filename);
+  if (file)
+  {
+    while (file >> userName)
+    {
+      file >> passWord;
+      table[userName] = passWord;
+    }
+  }
 }
 
 size_t UserLogin::numberOfUsers() {
 
-    // TO DO
-
+  // TO DO
+  return table.size();
 }
 
 string UserLogin::passWordCheck(const string& userName) {
 
-    // TO DO
-
+  // TO DO
+  std::unordered_map<std::string,std::string>::const_iterator user = table.find(userName);
+  if (user == table.end())
+  {
+    return "Non-existent User";
+  }
+  return user->second;
 }
 
 size_t UserLogin::wordBucketIdMethod1(const string& userName) {
 
     // TO DO
-
+    return table.bucket(userName);
 }
 
 size_t UserLogin::wordBucketIdMethod2(const string& userName) {
-    
-    // TO DO
-    
+
+  // TO DO
+  unsigned bucketNum = table.bucket_count();
+  for (unsigned i = 0; i < bucketNum; i++)
+  {
+    for (auto iterator = table.begin(i); iterator != table.end(i); iterator++)
+    {
+      if (iterator->first == userName)
+      {
+        return i;
+      }
+    }
+  }
 }
 
 bool UserLogin::validateUser(const string& userName) {
-    
-    // TO DO
-     
+
+  // TO DO
+  std::unordered_map<std::string,std::string>::const_iterator user = table.find(userName);
+  if (user == table.end())
+  {
+    return false;
+  }
+  return true;
 }
 
-bool UserLogin::authenticateUser(const string& userName, const string& passWord) { 
-    
-    // TO DO
-      
+bool UserLogin::authenticateUser(const string& userName, const string& passWord) {
+
+  // TO DO
+  std::unordered_map<std::string,std::string>::const_iterator user = table.find(userName);
+  if (user == table.end())
+  {
+    return false;
+  }
+  if (user->second == passWord)
+  {
+    return true;
+  }
+  return false;
  }
